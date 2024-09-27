ff6af82a148ba95fd21ce6307a5cf5a1a22865eb
vin <vin@vin.vin>
2024-09-27 08:35:01 +0530

    Changed the styling

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let countEl= document.getElementById('count-el');
+let saveEl= document.getElementById('save-el');
 
 let count=0;
 
 function increment(){
     count += 1;
-    countEl.innerText=count;
+    countEl.textContent=count;
 }
 
 
 function save (){
-    console.log(count);
+    let saveCount = count + " - ";
+    saveEl.textContent+= saveCount;
+    countEl.textContent= 0;
+    count=0;
 }
+
+
+
+
